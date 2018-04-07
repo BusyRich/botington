6066a76a635604ce48bf057c02c46d692d800a37
Busyrich <rich@busyrich.com>
2018-04-07 12:49:36 -0700

    Added alias support to the simple commands plugin

--- plugins/simple_command/scommand.js
+++ plugins/simple_command/scommand.js
@@ -4,17 +4,25 @@ module.exports = plugin => {
       this.commands = {}
       this.purchase = this.bot.plugins['member-points'].purchase;
 
+      const runCommand = (text, meta) => {
+        if(text[0] === '!') {
+          this.bot.runCommand(text.substring(1), meta);
+        } else {
+          this.bot.send(text);
+        }
+      };
+
       //One handler can be used for all commands, since they all just
       //display text.
       const handleCommand = meta => {
         if(this.commands[meta.command]) {
           let c = this.commands[meta.command];
           if(!c.cost || c.cost === 0) {
-            this.bot.send(c.text);
+            runCommand(c.text, meta);
           } else {
             purchase(meta.username, c.cost, success => {
               if(success) {
-                this.bot.send(c.text);
+                runCommand(c.text, meta);
               }
             });
           }
