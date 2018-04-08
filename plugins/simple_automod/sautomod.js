a59bda2bdcb402b8af913477d34b2d2f1d68b84e
Anthony Rivas <kamikazebot@gmail.com>
2018-04-07 22:47:10 -0700

    Initial automod ui - no functionality

--- plugins/simple_automod/sautomod.js
+++ plugins/simple_automod/sautomod.js
@@ -1,5 +1,5 @@
 module.exports = plugin => {
-  return plugin.bind( {
+  return plugin.bind({
     initialize() {
       this.restrictedWords = plugin.settings['restricted-words'];
       this.disableURLPosting = plugin.settings['remove-urls'];
@@ -19,68 +19,67 @@ module.exports = plugin => {
       //   ( err ) => console.log( error || 'Automod Flags Reset' )
       // );
 
-      this.bot.on( 'message', data => {
-        if ( !this.enabled ) {
+      this.bot.on('message', data => {
+        if (!this.enabled) {
           return;
         }
 
-        if ( ( data.meta.badges.broadcaster == '1'
-          && this.disableForBroadcaster === false )
-          || ( data.meta.badges.moderator == '1'
-            && this.disableForMods === false )
-          || ( data.meta.badges.broadcaster !== '1'
+        if ((data.meta.badges.broadcaster == '1'
+          && this.disableForBroadcaster === false)
+          || (data.meta.badges.moderator == '1'
+            && this.disableForMods === false)
+          || (data.meta.badges.broadcaster !== '1'
             && data.meta.badges.moderator !== '1'
-          ) ) {
-          var words = data.message.split( " " );
-          for ( let i = 0; i < words.length; i++ ) {
-            if ( this.restrictedWords.includes( words[i].toLowerCase() ) ) {
-              this.sendm( 'restricted', data );
-              this.updateStrikes( data.username, 'restricted' );
+          )) {
+          var words = data.message.split(" ");
+          for (let i = 0; i < words.length; i++) {
+            if (this.restrictedWords.includes(words[i].toLowerCase())) {
+              this.updateStrikes(data.username, 'restricted');
               return i;
-            } else if ( this.validURL( words[i] ) && this.disableURLPosting ) {
-              this.updateStrikes( data.username, 'urlRestricted' );
+            } else if (this.validURL(words[i]) && this.disableURLPosting) {
+              this.updateStrikes(data.username, 'urlRestricted');
             }
           }
         }
-      } );
-      this.event.emit( 'ready' );
+      });
+      this.event.emit('ready');
 
-      this.validURL = function ( str ) {
-        var pattern = new RegExp( '^(https?:\\/\\/)?' + // protocol
+      this.validURL = function (str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
           '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name and extension
           '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
           '(\\:\\d+)?' + // port
           '(\\/[-a-z\\d%@_.~+&:]*)*' + // path
           '(\\?[;&a-z\\d%@_.,~+&:=-]*)?' + // query string
-          '(\\#[-a-z\\d_]*)?$', 'i' ); // fragment locator
-        return pattern.test( str );
+          '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+        return pattern.test(str);
       };
-      this.updateStrikes = function ( user, message ) {
-        this.db.updateMember( {
+      this.updateStrikes = function (user, message) {
+        this.db.updateMember({
           username: user,
         }, {
             $inc: { automodStrikes: 1 }
-          }, ( error, result ) => {
-            if ( error ) {
-              this.event.emit( 'error', error );
+          }, (error, result) => {
+            if (error) {
+              this.event.emit('error', error);
             }
-            this.db.getMember( user, ( error, member ) => {
-              if ( error ) {
-                this.event.emit( 'error', error );
+            this.db.getMember(user, (error, member) => {
+              if (error) {
+                this.event.emit('error', error);
               }
 
-              if ( member && member.automodStrikes > this.progressiveTimeoutThreshold && this.progressiveTimeout ) {
-                this.sendm( 'progressive-timeout', { username: user, length: member.automodStrikes * this.progressiveTimeoutAmount } );
-                this.sendm( message, { username: user, count: member.automodStrikes } );
+              if (member && member.automodStrikes > this.progressiveTimeoutThreshold && this.progressiveTimeout) {
+                this.sendm('progressive-timeout', { username: user, length: member.automodStrikes * this.progressiveTimeoutAmount });
+                this.sendm(message, { username: user, count: member.automodStrikes });
               } else {
-                this.sendm( 'timeout', { username: user } );
-                this.sendm( message, { username: user, count: member.automodStrikes } );
+                this.sendm('timeout', { username: user });
+                this.sendm(message, { username: user, count: member.automodStrikes });
               }
-            } );
+            });
 
-          } );
+          });
       };
 
     }
-  } );
+  });
 };
--- plugins/simple_automod/ui/automod.vue
+++ plugins/simple_automod/ui/automod.vue
@@ -1,16 +1,142 @@
 <template>
   <div id="automod">
+    <h1>{{plugin.name}}: Control Panel</h1>
+    <div class="setting-module">
+      <h2>Ignore Streamer</h2>
+      <label class="switch">
+        <input type="checkbox" v-model="plugin['ignore-broadcaster']">
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <div class="setting-module">
+      <h2>Ignore Moderators</h2>
+      <label class="switch">
+        <input type="checkbox" v-model="plugin['ignore-mods']">
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <div class="setting-module">
+      <h2>Increasing Timeouts</h2>
+      <label class="switch">
+        <input type="checkbox" v-model="plugin['progressive-timeouts']">
+        <span class="slider round"></span>
+      </label>
+    </div>
+    <div class="setting-module">
+      <h2>Block URLs</h2>
+      <label class="switch">
+        <input type="checkbox" v-model="plugin['remove-urls']">
+        <span class="slider round"></span>
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
 module.exports = {
-  created() {},
-  data() {
-    plugin: bot.plugins["simple-automod"].config;
+  created() {
+    console.log(bot.plugins["simple-automod"].config);
+    this.updateData(bot.plugins["simple-automod"].config);
+  },
+  data: {
+    plugin: {}
+  },
+  methods: {
+    updateData(config) {
+      this.plugin = {
+        name: config["display-name"]
+      };
+      for (var key in config["default-settings"]) {
+        this.plugin[key] = config["default-settings"][key];
+      }
+    },
+    toggleSetting(setting) {
+      let s = 
+    },
+    updatePlugin(plugin) {
+      let p = bot.plugins["simple-automod"],
+        callback = error => {
+          if (error) {
+            return console.log(error);
+          }
+
+          this.updatePlugin(p);
+        };
+
+      if (p.enabled) {
+        p.disable(callback);
+      } else {
+        p.enable(callback);
+      }
+    }
   }
 };
 </script>
 
+<style lang="scss">
+#automod {
+  font-family: monospace;
+  padding: 10px;
 
+  .setting-module {
+    width: calc(50% - 20px);
+    min-width: 150px;
+    display: inline-block;
+    padding: 20px;
+  }
+
+  /* The switch - the box around the slider */
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
+  /* Hide default HTML checkbox */
+  .switch input {
+    display: none;
+  }
+  /* The slider */
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+  input:checked + .slider {
+    background-color: #2196f3;
+  }
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+  .slider.round:before {
+    border-radius: 50%;
+  }
+}
+</style>
 
