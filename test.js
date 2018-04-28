f085310c467fb29dd21f48490d4574bfb70b46dd
Busyrich <rich@busyrich.com>
2018-04-28 12:15:38 -0700

    Reorg. This repo is now only the bot core, no UI.

---
+++ test.js
@@ -0,0 +1,41 @@
+const path = require('path'),
+      url = require('url'),
+      Botington = require(__dirname + '/lib/' + 'bot'),
+      config = require(__dirname + '/../config.json');
+
+const bot = Botington(config);
+
+bot.on('channel', function(data) {
+  console.log('Connected to Channel #%s', data.channel);
+});
+
+bot.on('join', function(data) {
+  console.log('%s has joined the chat!', data.username);
+  console.log(bot.chatters.size);
+});
+
+bot.on('part', function(data) {
+  console.log('%s has left the chat. :(', data.username);
+  console.log(bot.chatters.size);
+});
+
+bot.on('message', function(data) {
+  console.log(data);
+});
+
+bot.on('command', function(data) {
+  console.log(data);
+});
+
+bot.on('names', function(data) {
+  console.log(data.users);
+  console.log(bot.chatters.size);
+});
+
+bot.on('ready', () => {
+});
+
+bot.on('refresh', () => {
+});
+
+bot.initialize();
