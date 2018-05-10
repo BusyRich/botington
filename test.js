fd33140454ea7ac532464e833d5660172bb3243c
Richard Key <rich@busyrich.com>
2018-05-09 18:43:35 -0700

    first pass on integrating BotBase and refactoring some objects.

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const path = require('path'),
       url = require('url'),
       Botington = require(__dirname + '/lib/' + 'bot');
 
-const bot = Botington(path.join(__dirname, '/../'));
+const bot = new Botington(path.join(__dirname, '/../'));
 
 bot.on('channel', function(data) {
   console.log('Connected to Channel #%s', data.channel);
@@ -35,6 +35,10 @@ bot.on('ready', () => {
 });
 
 bot.on('refresh', () => {
+});
+
+bot.registerCommand('butt', function(meta, ...args) {
+  bot.pm(meta.username, `I just wanted to let you know...butt, with ${args.length} arguments.`);
 });
 
 bot.initialize();
